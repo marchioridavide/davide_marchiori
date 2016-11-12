28a9ed9ab0fb35e7e6c942efa5f103e61a7a5ebe
utente <davide.melkior@gmail.com>
2016-11-12 11:39:41 +0100

    rectree

---
+++ recursivetree/recursivetree/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace recursivetree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(500, 566);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 750);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ recursivetree/recursivetree/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace recursivetree
+{
+    public partial class Form1 : Form
+    {
+        Pen p;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Black;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Black);
+            g = this.CreateGraphics();
+            
+        }
+        
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            drawTree(700, 550, -90.0, 9, 15F, 10);
+        }
+        private void drawTree(float x1, float y1, double theta, int depth, double dtheta, float scale)
+        {
+           
+                if (depth != 0)
+                {
+
+                    float x2 = (float)(x1 + (Math.Cos(theta * Math.PI/180.0) * scale * 10.0));     //si calcolano le x e le y trasformando l'angolo in radianti
+                    float y2 = (float)(y1 + (Math.Sin(theta * Math.PI/180.0) * scale * 10.0));     //scale rappresenta la scala, il che serve a diminuire la lunghezza dei rami ogni man mano che la funzione viene eseguita
+                                                                                                   //se scale è un valore inferiore rispetto a depth  diventa minore di 0, ciò significa che "andrà indietro"
+
+                    p.Color = Color.FromArgb(91, 148, 239);
+                    g.DrawLine(p, x1, y1, x2, y2);
+                    drawTree(x2, y2, theta - dtheta, depth - 1, dtheta,scale-1);             
+                    drawTree(x2, y2, theta + dtheta, depth - 1, dtheta,scale-1);
+
+                }
+
+        }
+    }
+}
